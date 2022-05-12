5315144f14c6e99bc80ca2c4a00b2d8dfde6ba48
Ana Ludmila <kn.ludi@gmail.com>
2022-05-12 14:25:00 -0300

    Including bitbucket fetch default reviewers action

--- lib/fastlane/plugin/bitbucket/actions/bitbucket_fetch_default_reviewers_action.rb
+++ lib/fastlane/plugin/bitbucket/actions/bitbucket_fetch_default_reviewers_action.rb
@@ -4,14 +4,18 @@ module Fastlane
       BITBUCKET_FETCH_REVIEWERS_ACTION_CUSTOM_VALUE = :BITBUCKET_FETCH_REVIEWERS_ACTION_CUSTOM_VALUE
     end
 
-    class BitbucketFetchDefaultReviewersActionAction < Action
+    class BitbucketFetchDefaultReviewersAction < Action
       def self.run(params)
-        # fastlane will take care of reading in the parameter and fetching the environment variable:
-        UI.message "Parameter API Token: #{params[:api_token]}"
+        auth_header = Helper::BitbucketHelper.get_auth_header(params)
+        
+        if params[:base_url] then
+          base_url = params[:base_url]
+        else
+          base_url = 'https://api.bitbucket.org'
+        end
 
-        # sh "shellcommand ./path"
+        Helper::BitbucketHelper.fetch_default_reviewers(auth_header, base_url, params[:project_key], params[:repo_slug])
 
-        # Actions.lane_context[SharedValues::BITBUCKET_FETCH_REVIEWERS_ACTION_CUSTOM_VALUE] = "my_val"
       end
 
       def self.description
@@ -69,34 +73,8 @@ module Fastlane
         ]
       end
 
-      def self.output
-        # Define the shared values you are going to provide
-        # Example
-        [
-          ['BITBUCKET_FETCH_REVIEWERS_ACTION_CUSTOM_VALUE', 'A description of what this value contains']
-        ]
-      end
-
-      def self.return_value
-        # If your method provides a return value, you can describe here what it does
-      end
-
-      def self.authors
-        # So no one will ever forget your contribution to fastlane :) You are awesome btw!
-        ["Your GitHub/Twitter Name"]
-      end
-
       def self.is_supported?(platform)
-        # you can do things like
-        #
-        #  true
-        #
-        #  platform == :ios
-        #
-        #  [:ios, :mac].include?(platform)
-        #
-
-        platform == :ios
+        true
       end
     end
   end
